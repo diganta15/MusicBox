464e6ab6b57ca5809630153fcdc00f71da8ec291
diganta15 <ddas461598@gmail.com>
2021-07-27 00:09:29 +0530

    Added some styling to genres

--- musicbox-web/src/components/layout/categories/CategoryItem.js
+++ musicbox-web/src/components/layout/categories/CategoryItem.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
 const CategoryItem = ({category}) => {
+
+    const backgroundImg = {
+        backgroundImage:`url(${category.icons[0].url})`,
+        backgroundRepeat:'no-repeat',
+     
+        backgroundSize:'cover',
+    
+    }
+
+
     return (
-        <div>
-            <h1>{category.name}</h1>
+        <div className="category-item pointer" style={backgroundImg}  >
+            <div className="category-wrapper">
+                <h1 className="category-name">{category.name}</h1>
+            </div>
         </div>
     )
 }
+
+
 
 export default CategoryItem
